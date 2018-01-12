25853c460951aa9dc68bf34305e07270cae14bf2
Feng0923 <1216116203@qq.com>
2018-01-12 17:45:00 +0800

    submit

--- java_proxy/src/cglib_proxy/BookFacadeCglib.java
+++ java_proxy/src/cglib_proxy/BookFacadeCglib.java
@@ -1,14 +1,13 @@
 package cglib_proxy;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.cglib.proxy.Callback;
 import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodProxy;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandleInfo;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-public class BookFacadeCglib implements MethodInterceptor {
+public class BookFacadeCglib implements org.springframework.cglib.proxy.MethodInterceptor{
     private Object target;
 
     /**
@@ -20,11 +19,19 @@ public class BookFacadeCglib implements MethodInterceptor {
         this.target = target;
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(this.target.getClass());
-        enhancer.setCallback((Callback) this); //回调方法
+        enhancer.setCallback(this); //回调方法
         return enhancer.create();  //创建代理对象
     }
+
+    /*
+    回调方法
+    调用此方法的类就是Callback类,也就是代理类BookFacadeCglib自身通过Enhancer的setCallback方法进行设置
+     */
     @Override
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("事物开始");
+        methodProxy.invokeSuper(o,objects);//调用super也就是被代理类的方法
+        System.out.println("事物结束");
         return null;
     }
 }
